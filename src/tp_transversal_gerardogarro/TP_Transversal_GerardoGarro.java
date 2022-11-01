9514843fce9d53233fb8be731cc6e78553b69e01
GeraGarro <geragarro@gmail.com>
2022-10-31 23:06:30 -0300

    Inicio del Proyecto Transversal

---
+++ src/tp_transversal_gerardogarro/TP_Transversal_GerardoGarro.java
@@ -0,0 +1,17 @@
+
+package tp_transversal_gerardogarro;
+
+/**
+ *
+ * @author pc
+ */
+public class TP_Transversal_GerardoGarro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
